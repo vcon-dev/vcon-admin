74dfad04f2fa8843798b6f7fb903f3194129bd8b
Thomas Howe <ghostofbasho@gmail.com>
2024-06-21 17:42:25 -0400

    With updated formatting

--- pages/chatgpt_files.py
+++ pages/chatgpt_files.py
@@ -7,9 +7,9 @@ from openai import OpenAI
 import time
 
 client = OpenAI(
-  organization=st.secrets["openai"]["organization"],
-  project=st.secrets["openai"]["project"],
-  api_key=st.secrets["openai"]["testing_key"]
+    organization=st.secrets["openai"]["organization"],
+    project=st.secrets["openai"]["project"],
+    api_key=st.secrets["openai"]["testing_key"],
 )
 common.init_session_state()
 common.authenticate()
@@ -22,121 +22,120 @@ st.title("OpenAI")
 # Make a tab group for the different actions
 tabs = ["Upload Files", "Download Files", "Delete Files", "List Files"]
 upload, download, delete, list_files = st.tabs(tabs)
-  
+
 with upload:
-  "Upload the vCons to OpenAI. You can choose the purpose of the files, such as 'assistants', 'batch', 'fine-tune', etc."
+    "Upload the vCons to OpenAI. You can choose the purpose of the files, such as 'assistants', 'batch', 'fine-tune', etc."
 
-  file_purposes = [
-      "assistants",
-      "assistants_output",
-      "batch",
-      "batch_output",
-      "fine-tune",
-      "fine-tune-results",
-  ]
-  purpose = st.selectbox("File Purpose", file_purposes)
+    file_purposes = [
+        "assistants",
+        "assistants_output",
+        "batch",
+        "batch_output",
+        "fine-tune",
+        "fine-tune-results",
+    ]
+    purpose = st.selectbox("File Purpose", file_purposes)
 
-  # Make a dropdown for the vector store. Use the API to get the vector stores, and then populate the dropdown
-  vector_stores = client.beta.vector_stores.list()
+    # Make a dropdown for the vector store. Use the API to get the vector stores, and then populate the dropdown
+    vector_stores = client.beta.vector_stores.list()
 
-  # Use the human readable name for the dropdown
-  vector_store_names = [vector_store.name for vector_store in vector_stores.data]
-  vector_store_id = st.selectbox("Vector Store", vector_store_names)
+    # Use the human readable name for the dropdown
+    vector_store_names = [vector_store.name for vector_store in vector_stores.data]
+    vector_store_id = st.selectbox("Vector Store", vector_store_names)
 
-  # Convert the human readable name to the ID
-  vector_store_id = vector_stores.data[vector_store_names.index(vector_store_id)].id
-  with st.expander("Vector Store Details"):
-      vector_store = client.beta.vector_stores.retrieve(vector_store_id)
-      st.write(f"**Name**: {vector_store.name}")
-      st.write(f"**File Counts**:")
-      st.write(f"  _In Progress_: {vector_store.file_counts.in_progress}")
-      st.write(f"  _Completed_: {vector_store.file_counts.completed}")
-      st.write(f"  _Failed_: {vector_store.file_counts.failed}")
-      st.write(f"  _Cancelled_: {vector_store.file_counts.cancelled}")
-      st.write(f"  **Total**: {vector_store.file_counts.total}")
-      
-      
-  # Upload the vCons
-  upload = st.button("Upload vCons to OpenAI")
-  if upload:
-      vcons = common.get_vcons()
-      files = []
-      with st.status(f"Uploading vcons to vector store") as status:
-        st.write(f"Uploading {len(vcons)} vcons to OpenAI.")
-        for i, vcon in enumerate(vcons):
-          # Save the vCon as a file
-          file_name = f'{vcon["uuid"]}.vcon.json'
-          with open(file_name, "w") as f:
-              f.write(json.dumps(vcon))
-              
-          # Upload the file to OpenAI
-          file = client.files.create(file=open(file_name, "rb"), purpose=purpose)
-          files.append(file)      
-          # Remove the file
-          os.remove(file_name)
-        st.write(f"Files uploaded to OpenAI.")
-        st.write(f"Adding files to vector store")     
-        for file in files:
-          vector_store_file = client.beta.vector_stores.files.create(
-            vector_store_id=vector_store_id,
-            file_id=file.id
-          )
-        st.write(f"Files added to vector store {vector_store_id}")
-        st.write(f"Files uploaded to OpenAI.")
-  
+    # Convert the human readable name to the ID
+    vector_store_id = vector_stores.data[vector_store_names.index(vector_store_id)].id
+    with st.expander("Vector Store Details"):
+        vector_store = client.beta.vector_stores.retrieve(vector_store_id)
+        st.write(f"**Name**: {vector_store.name}")
+        st.write(f"**File Counts**:")
+        st.write(f"  _In Progress_: {vector_store.file_counts.in_progress}")
+        st.write(f"  _Completed_: {vector_store.file_counts.completed}")
+        st.write(f"  _Failed_: {vector_store.file_counts.failed}")
+        st.write(f"  _Cancelled_: {vector_store.file_counts.cancelled}")
+        st.write(f"  **Total**: {vector_store.file_counts.total}")
+
+    # Upload the vCons
+    upload = st.button("Upload vCons to OpenAI")
+    if upload:
+        vcons = common.get_vcons()
+        files = []
+        with st.status(f"Uploading vcons to vector store") as status:
+            st.write(f"Uploading {len(vcons)} vcons to OpenAI.")
+            for i, vcon in enumerate(vcons):
+                # Save the vCon as a file
+                file_name = f'{vcon["uuid"]}.vcon.json'
+                with open(file_name, "w") as f:
+                    f.write(json.dumps(vcon))
+
+                # Upload the file to OpenAI
+                file = client.files.create(file=open(file_name, "rb"), purpose=purpose)
+                files.append(file)
+                # Remove the file
+                os.remove(file_name)
+            st.write(f"Files uploaded to OpenAI.")
+            st.write(f"Adding files to vector store")
+            for file in files:
+                vector_store_file = client.beta.vector_stores.files.create(
+                    vector_store_id=vector_store_id, file_id=file.id
+                )
+            st.write(f"Files added to vector store {vector_store_id}")
+            st.write(f"Files uploaded to OpenAI.")
+
 with download:
-  "Download the files from OpenAI to your local machine."
+    "Download the files from OpenAI to your local machine."
 
-  # Download the vCons
-  download = st.button("Download")
-  if download:
-    # Pick the destination folder
-    st.write("Select the destination folder")
-    destination = st.text_input("Destination", value=".")
-    if not os.path.exists(destination):
-        os.makedirs(destination)
-    os.chdir(destination)
-    
-    st.write("Downloading files from OpenAI")
-    with st.spinner("Downloading files from OpenAI"):
-        files = client.files.list(purpose=purpose)
-        for file in files:
-            file_name = file["name"]
-            with open(file_name, "wb") as f:
-                f.write(file.get_content())
-    st.success("Files downloaded successfully")
+    # Download the vCons
+    download = st.button("Download")
+    if download:
+        # Pick the destination folder
+        st.write("Select the destination folder")
+        destination = st.text_input("Destination", value=".")
+        if not os.path.exists(destination):
+            os.makedirs(destination)
+        os.chdir(destination)
 
-with delete: 
-  "This will delete all the files in the selected purpose. Be careful, as this action cannot be undone."
+        st.write("Downloading files from OpenAI")
+        with st.spinner("Downloading files from OpenAI"):
+            files = client.files.list(purpose=purpose)
+            for file in files:
+                file_name = file["name"]
+                with open(file_name, "wb") as f:
+                    f.write(file.get_content())
+        st.success("Files downloaded successfully")
 
+with delete:
+    "This will delete all the files in the selected purpose. Be careful, as this action cannot be undone."
 
-  delete_files = st.button("Delete Files from OpenAI")
-  if delete_files:
-    progress_text = "Deleting files from OpenAI."
-    with st.progress(0, text=progress_text):
-      files = client.files.list(purpose=purpose)
-      for i, file in enumerate(files):
-        client.files.delete(file_id=file.id)
-        st.progress((i+1)/len(files))
-    st.success("Files deleted successfully")
-    
+    delete_files = st.button("Delete Files from OpenAI")
+    if delete_files:
+        progress_text = "Deleting files from OpenAI."
+        with st.progress(0, text=progress_text):
+            files = client.files.list(purpose=purpose)
+            for i, file in enumerate(files):
+                client.files.delete(file_id=file.id)
+                st.progress((i + 1) / len(files))
+        st.success("Files deleted successfully")
+
 with list_files:
-  "List all of the files currently uploaded. This will show the file name and the file size."
+    "List all of the files currently uploaded. This will show the file name and the file size."
 
-  show_files = st.button("Fetch")
-  if show_files:
-      resp = client.files.list(purpose=purpose)
-      files = resp.data
-      st.metric("Number of Files", len(files))
-      with st.expander("Details"):
-        for file in files:
-            st.write(f"{file.filename} - {file.bytes} bytes - {datetime.fromtimestamp(file.created_at)}")
+    show_files = st.button("Fetch")
+    if show_files:
+        resp = client.files.list(purpose=purpose)
+        files = resp.data
+        st.metric("Number of Files", len(files))
+        with st.expander("Details"):
+            for file in files:
+                st.write(
+                    f"{file.filename} - {file.bytes} bytes - {datetime.fromtimestamp(file.created_at)}"
+                )
 
 st.divider()
 
 "## Assistant Testing"
 
-today = datetime.today().strftime('%Y-%m-%d')
+today = datetime.today().strftime("%Y-%m-%d")
 
 # Make a list of the available assistants
 assistants = client.beta.assistants.list()
@@ -146,16 +145,15 @@ assistant_name = st.selectbox("Assistant", assistant_names)
 # Show the assistant details. Get the assistant by name
 assistant = assistants.data[assistant_names.index(assistant_name)]
 with st.expander("Assistant Details"):
-  # Make a human readable date
-  created_at = datetime.fromtimestamp(assistant.created_at)
-  st.write(f"**Name**: {assistant.name}")
-  st.write(f"**ID**: {assistant.id}")
-  st.write(f"**Model**: {assistant.model}")
-  st.write(f"**Instructions**: {assistant.instructions}")
-  st.write(f"*Description**: {assistant.description}")
-  st.write(f"**Tools**: {assistant.tools}")
-  st.write(f"**Created At**: {created_at}")
-
+    # Make a human readable date
+    created_at = datetime.fromtimestamp(assistant.created_at)
+    st.write(f"**Name**: {assistant.name}")
+    st.write(f"**ID**: {assistant.id}")
+    st.write(f"**Model**: {assistant.model}")
+    st.write(f"**Instructions**: {assistant.instructions}")
+    st.write(f"*Description**: {assistant.description}")
+    st.write(f"**Tools**: {assistant.tools}")
+    st.write(f"**Created At**: {created_at}")
 
 
 with st.spinner("Connecting to OpenAI..."):
@@ -169,11 +167,11 @@ with st.spinner("Connecting to OpenAI..."):
         message = client.beta.threads.messages.create(
             thread_id=st.session_state.thread.id,
             role="user",
-            content="Today is " + today
+            content="Today is " + today,
         )
 
-    
-# In the sidebar, show the vConIDs that are used in this conversation, 
+
+# In the sidebar, show the vConIDs that are used in this conversation,
 # and provide a link to the vCon detail page using the vConID and CONV_DETAIL_URL
 # Get the thread
 thread = client.beta.threads.retrieve(thread_id=st.session_state.thread.id)
@@ -192,45 +190,36 @@ if prompt := st.chat_input("How can I help you?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
 
     message = client.beta.threads.messages.create(
-        thread_id=thread.id,
-        role="user",
-        content=prompt
+        thread_id=thread.id, role="user", content=prompt
     )
 
     run = client.beta.threads.runs.create(
-        thread_id=thread.id,
-        assistant_id=assistant.id
-        )
-    
+        thread_id=thread.id, assistant_id=assistant.id
+    )
+
     # Wait for the run to complete
     # Show a progress bar while waiting
     status_bar = st.status("Processing...")
     with status_bar:
-        status = client.beta.threads.runs.retrieve(
-            thread_id=thread.id,
-            run_id=run.id
-        )
+        status = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
         while status.status != "completed":
             time.sleep(1)  # Add a 1-second delay between status retrievals
             status = client.beta.threads.runs.retrieve(
-                thread_id=thread.id,
-                run_id=run.id
+                thread_id=thread.id, run_id=run.id
             )
 
     # Print out the run step object
-    steps = client.beta.threads.runs.steps.list(
-        thread_id=thread.id,
-        run_id=run.id
-    )
+    steps = client.beta.threads.runs.steps.list(thread_id=thread.id, run_id=run.id)
     for step in steps:
         step_details = step.step_details
         if step_details.type == "message_creation":
             message_id = step.step_details.message_creation.message_id
             message = client.beta.threads.messages.retrieve(
-                thread_id=thread.id,
-                message_id=message_id
+                thread_id=thread.id, message_id=message_id
             )
             message_content = message.content[0].text
             with st.chat_message("assistant"):
                 st.markdown(message_content.value)
-            st.session_state.messages.append({"role": "assistant", "content": message_content.value})
+            st.session_state.messages.append(
+                {"role": "assistant", "content": message_content.value}
+            )
--- pages/vcons.py
+++ pages/vcons.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pymongo
-import json
 import lib.common as common
-import pandas as pd
 import pandas as pd
 
 common.init_session_state()
@@ -15,6 +13,7 @@ def get_mongo_client():
     url = st.secrets["mongo_db"]["url"]
     return pymongo.MongoClient(url)
 
+
 def parties_as_markdown(parties):
     # {"tel": "+19981076214", "meta": {"role": "customer"}, "name": "Janet Davis", "email": "janet.davis@gmail.com"}
     # Convert the party to a markdown string, including all of the information,
@@ -24,7 +23,7 @@ def parties_as_markdown(parties):
         role = party.get("meta", {}).get("role", "")
         # Upper case the role
         role = role.upper()
-        party_str += f"{role} - {party.get('name', '')} {party.get('tel', '')} {party.get('email', '')}"   
+        party_str += f"{role} - {party.get('name', '')} {party.get('tel', '')} {party.get('email', '')}"
     return party_str
 
 
@@ -35,7 +34,7 @@ vcon_count = collection.count_documents({})
 if vcon_count == 0:
     st.error("No VCONs found in the database")
     st.stop()
-    
+
 vcons = collection.find({})
 
 st.title(f"VCON INFO: {vcon_count} vcons")
@@ -44,7 +43,7 @@ st.title(f"VCON INFO: {vcon_count} vcons")
 items_per_page = 10
 
 # Streamlit session state to keep track of the current page
-if 'page' not in st.session_state:
+if "page" not in st.session_state:
     st.session_state.page = 0
 
 # Pagination controls
@@ -68,12 +67,14 @@ for col in to_remove:
 df["created_at"] = df["created_at"].apply(lambda x: pd.to_datetime(x))
 
 # Extract the from each dialog object the total duration
-df["total_duration"] = df["dialog"].apply(lambda x: sum([int(dialog["duration"]) for dialog in x]))
+df["total_duration"] = df["dialog"].apply(
+    lambda x: sum([int(dialog["duration"]) for dialog in x])
+)
 
 # Extract the names, tel and email addresses from parties
 df["party_names"] = df["parties"].apply(lambda x: parties_as_markdown(x))
 
-df['link'] = df['uuid'].apply(lambda x: f'<a href="/inspect?uuid={x}">Details</a>')
+df["link"] = df["uuid"].apply(lambda x: f'<a href="/inspect?uuid={x}">Details</a>')
 
 df = df.drop(columns=["parties"])
 df = df.drop(columns=["dialog"])
@@ -83,7 +84,7 @@ optional_columns = ["redacted", "group", "appended"]
 for col in optional_columns:
     if col in df.columns:
         df = df.drop(columns=col)
-        
+
 
 # Display dataframe with clickable links
 st.markdown(df.to_html(escape=False), unsafe_allow_html=True)
@@ -94,7 +95,7 @@ with col1:
         st.session_state.page -= 1
 with col2:
     st.write(f"Page {st.session_state.page + 1} of {total_pages}")
-    
+
 with col3:
     if st.button("Next") and st.session_state.page < total_pages - 1:
         st.session_state.page += 1
